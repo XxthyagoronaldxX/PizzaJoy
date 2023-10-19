874e5406b4e0d36cab61f39536599363f5d41374
XxthyagoronaldxX <thyago.silva@netbiis.com>
2023-10-19 12:47:06 -0300

    New Class for Finder another Classes

--- src/main/java/com/danthy/pizzafun/app/utils/PathUtil.java
+++ src/main/java/com/danthy/pizzafun/app/utils/PathUtil.java
@@ -1,6 +1,7 @@
 package com.danthy.pizzafun.app.utils;
 
 import java.io.File;
+import java.nio.file.FileStore;
 
 public class PathUtil {
     public static String getRootPath() {
@@ -8,5 +9,12 @@ public class PathUtil {
                 "src" +
                 File.separator +
                 "main";
+    }
+
+    public static String getTargetPath() {
+        return System.getProperty("user.dir") + File.separator +
+                "target" +
+                File.separator +
+                "classes";
     }
 }
